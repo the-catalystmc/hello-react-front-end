c00c98acbfb1d788dbc1755ea1d6e2c8a3a2bb1e
Rhyine <rhyinestewart@gmail.com>
2021-11-30 16:02:42 -0500

    Add app entry to website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import PropTypes from "prop-types"
+import { Provider } from "react-redux"
+import store from "./configureStore"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Greeting from "./components/Greeting"
+class App extends React.Component {
+  render () {
+    return (
+      <Provider store={store}>
+      <div className="cont">
+        <Greeting />
+      </div>
+      </Provider>
+
+    );
+  }
 }
 
-export default App;
+App.propTypes = {
+  greeting: PropTypes.string
+};
+export default App
